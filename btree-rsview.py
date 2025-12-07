524161108996fbbe95d33b03e30e3f532634edb2
Nobodit Choudhury <nc3610@nyu.edu>
2025-12-07 18:45:32 -0500

    Add rightSideView method to Solution class
    Implement right side view of a binary tree using BFS.

---
+++ btree-rsview.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        queue = deque([root])
+        ans = list()
+        while queue:
+            nodes_at_level = len(queue)
+            ans.append(queue[-1].val)
+            for _ in range(nodes_at_level):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return ans
+                
+    
+        
